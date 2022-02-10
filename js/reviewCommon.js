88fc29e4121a71a4fd63ed4ef9a6c02e9f3d8c9d
jturner <jturner@ebi.ac.uk>
2022-02-10 13:51:27 +0000

    Troubleshooting
    Signed-off-by: jturner <jturner@ebi.ac.uk>

--- js/reviewCommon.js
+++ js/reviewCommon.js
@@ -180,7 +180,7 @@ function display_mol_star(molecule_url = 'undefined', primary_contour_level = 1,
                 }
             );
         }
-        /*if (map_xray_1_url !== 'undefined' || 'None') {
+        if (map_xray_1_url !== 'undefined' || 'None') {
             viewerInstance.loadVolumeFromUrl(
                 {
                     url: map_xray_1_url,
@@ -213,7 +213,7 @@ function display_mol_star(molecule_url = 'undefined', primary_contour_level = 1,
                     entryId: ['2FO-FC', 'FO-FC'],
                 }
             );
-        }*/
+        }
     });
 }
 
